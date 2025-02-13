e6f55cc52c28a728b77329eb540cc89ca96c44fb
Anshijindal20 <jindalanshika98@gmail.com>
2025-02-13 17:23:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PhoneForm from './components/PhoneForm';
+import './styles.css';  // Import the CSS file here
+
+function App() {
+  return (
+    <div className="App">
+      <PhoneForm />
+    </div>
+  );
+}
+
+export default App;
+
